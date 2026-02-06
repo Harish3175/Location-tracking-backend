4e47ad536be7fdacf454cdc61b5c03825d007a0d
HARISH S <harishofficial315@gmail.com>
2026-02-06 11:39:07 +0530

    fixed

--- server.cjs
+++ server.cjs
@@ -35,5 +35,5 @@ app.get("/",(req,res) => {
 // start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
+    console.log("Server running on port",PORT);
 });
